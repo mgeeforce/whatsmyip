d7c34aea21eb93ea456ae2a6a463df9b97dd7894
Mike Gee <mike@legolas2.local>
2017-07-31 14:40:40 -0600

    added support for clientUrl and split config files

--- app/io/adeptus/whatismyip/services/Ipify.java
+++ app/io/adeptus/whatismyip/services/Ipify.java
@@ -5,15 +5,19 @@ import java.util.concurrent.CompletionStage;
 import javax.inject.Inject;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.typesafe.config.Config;
 
 import play.libs.ws.*;
 
 public class Ipify {
 	private final WSClient ws;
+	private final Config config;
+
 
     @Inject
-    public Ipify(WSClient ws) {
+    public Ipify(WSClient ws, Config config) {
         this.ws = ws;
+        this.config = config;
     }
 
     public CompletionStage<JsonNode> call() {     
@@ -21,6 +25,6 @@ public class Ipify {
     }
    
     public CompletionStage<WSResponse> post() {
-    	return call().thenCompose(response -> ws.url("http://localhost:9000").post(response));
+    	return call().thenCompose(response -> ws.url(config.getString("clientUrl")).post(response));
     }
 }
